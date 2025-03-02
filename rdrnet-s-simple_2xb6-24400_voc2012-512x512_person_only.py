dbb195b42318eebbbb18f8036debf8e43448e142
pinto0309 <rmsdh122@yahoo.co.jp>
2025-03-02 18:36:37 +0900

    batch_size=8 -> batch_size=16

--- rdrnet-s-simple_2xb6-24400_voc2012-512x512_person_only.py
+++ rdrnet-s-simple_2xb6-24400_voc2012-512x512_person_only.py
@@ -89,7 +89,7 @@ tta_pipeline = [
         ])
 ]
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=16,
     num_workers=8,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
